780f7e25b8b6778d8b4c63bce3379982fd95c009
Paweł Hertman <pawel@hertman.eu>
2016-03-16 13:42:11 +0100

    Backend: listening to socket events

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
-var http = require("http");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+// @todo remove hello world
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', function (socket){
+  console.log('a user connected');
+});
+
+http.listen(8888, function(){
+  console.log('listening on *:8888');
+});
